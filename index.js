8b7ac441eaec33ceccde735c511f85c5acdcfa61
Jen <jen@meatspac.es>
2014-08-28 16:01:19 -0700

    initial validator for revisit

--- index.js
+++ index.js
@@ -1,7 +1,121 @@
 'use strict';
 
-var RevisitTether = function () {
-  this.services = [];
+var request = require('request');
+var dataURI = require('data-uri-to-buffer');
 
+var RevisitValidator = function (options) {
+  this.url = false;
+  this.content = {};
+  this.imgBuffer = false;
 
+  if (!options) {
+    options = {};
+  }
+
+  this.maxSize = parseInt(options.maxSize, 10) || 1000000; // 1 MB
+
+  this.errors = {};
+
+  var self = this;
+
+  var validateSize = function (next) {
+    try {
+      self.imgBuffer = dataURI(self.content.data);
+
+      if (['image/jpeg', 'image/png', 'image/gif'].indexOf(self.imgBuffer.type) === -1) {
+        self.errors.InvalidFileType = 'This can only be a PNG, JPEG or GIF';
+        next();
+        return;
+      }
+
+      if (self.imgBuffer.length > self.maxSize) {
+        self.errors.FileTooLarge = 'This image is greater than ' +
+          Math.floor(self.maxSize / 1000000) + ' MB';
+        next();
+        return;
+      }
+    } catch (e) {
+      self.errors.InvalidDataURI = "This image isn't a valid data URI";
+      next();
+      return;
+    }
+
+    next();
+  };
+
+  var checkPost = function (next) {
+    request({
+      method: 'POST',
+      json: true,
+      url: self.url + '/service',
+      body: {
+        content: {
+          data: self.imgDataURI
+        },
+        meta: {
+          audio: {}
+        }
+      }
+    }, function (err, response, body) {
+        if (!body || (body && !body.content)) {
+          self.errors.InvalidServicePost = 'Your POST request must point to /service';
+          next();
+          return;
+        }
+
+        self.content = body.content;
+
+        if (!body.meta) {
+          self.errors.InvalidServiceMeta = 'You need to carry the `meta` object in the request';
+          next();
+          return;
+        } else if (!body.meta.audio) {
+          self.errors.InvalidServiceMetaEmtpy = 'You should not destroy incoming data from `meta`';
+          next();
+          return;
+        }
+
+        validateSize(next);
+    });
+  };
+
+  var checkHead = function (next) {
+    request({
+      method: 'HEAD',
+      url: self.url,
+      followAllRedirects: false,
+      timeout: 2000
+    }, function (err, response) {
+      if (err || !response.statusCode || response.statusCode !== 200) {
+        self.errors.InvalidHeadRequest = 'Cannot make a HEAD request to /';
+        next();
+        return;
+      }
+
+      checkPost(next);
+    });
+  };
+
+  var checkURL = function () {
+    if (!self.url) {
+      self.errors.InvalidURL = 'URL must not be empty';
+    } else if (self.url[self.url.length - 1] === '/') {
+      self.errors.InvalidTrailingSlash = 'URL cannot contain a trailing slash';
+    }
+  };
+
+  this.validate = function (next) {
+    this.errors = {};
+
+    checkURL();
+
+    if (!this.errors.InvalidURL && !this.errors.InvalidTrailingSlash) {
+      checkHead(next);
+      return;
+    }
+
+    next();
+  };
 };
+
+module.exports = RevisitValidator;
--- test/test.index.js
+++ test/test.index.js
@@ -4,3 +4,189 @@ process.env.NODE_ENV = 'test';
 
 var should = require('should');
 var nock = require('nock');
+
+var RevisitValidator = require('../index');
+var rv = new RevisitValidator({
+  maxSize: 100
+});
+
+var url = 'http://test.com';
+var content = {
+  content: {
+    data: ''
+  }
+};
+
+var scope;
+var scopePost;
+
+var headReq = function () {
+  scope = nock(url)
+    .head('/')
+    .reply(200, 'Found');
+};
+
+var postReq = function () {
+  scopePost = nock(url)
+      .post('/service')
+      .reply(200, content);
+};
+
+describe('RevisitValidator', function () {
+  it('should return with an invalid empty URL', function (done) {
+    rv.validate(function () {
+      should.exist(rv.errors.InvalidURL);
+      done();
+    });
+  });
+
+  it('should return with an invalid URL containing a trailing slash', function (done) {
+    rv.url = url + '/';
+
+    rv.validate(function () {
+      should.exist(rv.errors.InvalidTrailingSlash);
+      done();
+    });
+  });
+
+  it('should return with an unsuccessful HEAD request at /', function (done) {
+    rv.url = url;
+
+    scope = nock(url)
+      .head('/')
+      .reply(404, "Not found");
+
+    rv.validate(function () {
+      should.exist(rv.errors.InvalidHeadRequest);
+      done();
+    });
+  });
+
+  it('should return with a successful HEAD request at /', function (done) {
+    headReq();
+    postReq();
+
+    rv.validate(function () {
+      should.not.exist(rv.errors.InvalidHeadRequest);
+      done();
+    });
+  });
+
+  it('should return with an invalid POST request at /service', function (done) {
+    headReq();
+
+    scope = nock(url)
+      .post('/service')
+      .reply(404, 'Not Found');
+
+    rv.validate(function () {
+      should.exist(rv.errors.InvalidServicePost);
+      done();
+    });
+  });
+
+  it('should return with a valid POST request at /service', function (done) {
+    headReq();
+    postReq();
+
+    rv.validate(function () {
+      should.not.exist(rv.errors.InvalidServicePost);
+      done();
+    });
+  });
+
+  it('should return with an invalid meta object', function (done) {
+    headReq();
+    postReq();
+
+    rv.validate(function () {
+      should.exist(rv.errors.InvalidServiceMeta);
+      done();
+    });
+  });
+
+  it('should return with a valid meta object', function (done) {
+    content.meta = {
+      audio: {}
+    };
+
+    headReq();
+    postReq();
+
+    rv.validate(function () {
+      should.not.exist(rv.errors.InvalidServiceMeta);
+      done();
+    });
+  });
+
+  it('should return with an invalid data URI', function (done) {
+    content.meta = {
+      audio: {}
+    };
+
+    headReq();
+    postReq();
+
+    rv.validate(function () {
+      should.exist(rv.errors.InvalidDataURI);
+      done();
+    });
+  });
+
+  it('should return with a valid data URI with a file size that is too big', function (done) {
+    content = {
+      content: {
+        data: 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQAB' + Array(1000).join('A') + '='
+      },
+      meta: {
+        audio: {}
+      }
+    };
+
+    headReq();
+    postReq();
+
+    rv.validate(function () {
+      should.exist(rv.errors.FileTooLarge);
+      done();
+    });
+  });
+
+  it('should return with an invalid file type', function (done) {
+    content = {
+      content: {
+        data: 'data:image/tiff;base64,/9j/4AAQSkZJRgABAQAAAQAB' + Array(10).join('A') + '='
+      },
+      meta: {
+        audio: {}
+      }
+    };
+
+    headReq();
+    postReq();
+
+    rv.validate(function () {
+      should.exist(rv.errors.InvalidFileType);
+      done();
+    });
+  });
+
+  it('should return with a valid data URI', function (done) {
+    content = {
+      content: {
+        data: 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQAB' + Array(10).join('A') + '='
+      },
+      meta: {
+        audio: {}
+      }
+    };
+
+    headReq();
+    postReq();
+
+    rv.validate(function () {
+      rv.errors.should.eql({});
+      done();
+    });
+  });
+});
